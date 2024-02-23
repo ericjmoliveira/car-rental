3d15fbf187c860d0e522bf5c775854be299430fa
Eric Oliveira <ericjmoliveira@gmail.com>
2024-02-23 19:49:47 -0300

    feat: create data fetching functions

---
+++ src/lib/data.ts
@@ -0,0 +1,51 @@
+import { prisma } from './prisma';
+
+export async function fetchCarClasses() {
+  try {
+    const carClasses = await prisma.class.findMany();
+
+    return carClasses;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchCarClassById(id: string) {
+  try {
+    const carClasses = await prisma.class.findUnique({ where: { id } });
+
+    return carClasses;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchLocations() {
+  try {
+    const locations = await prisma.location.findMany();
+
+    return locations;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchUserReservations(userId: string) {
+  try {
+    const userReservations = await prisma.reservation.findMany({ where: { userId } });
+
+    return userReservations;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchReservationById(id: string) {
+  try {
+    const reservation = await prisma.reservation.findUnique({ where: { id } });
+
+    return reservation;
+  } catch (error) {
+    console.log(error);
+  }
+}
