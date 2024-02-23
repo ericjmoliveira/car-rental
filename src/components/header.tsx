7f3280ebc23e1267235e1baf7bdc2ac4b5050d6b
Eric Oliveira <ericjmoliveira@gmail.com>
2024-02-23 20:55:09 -0300

    feat: add sign in/out buttons to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,41 @@
+'use client';
+
 import { CarFront, CircleUserRound } from 'lucide-react';
 
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from './ui/button';
+import { signIn, useSession } from 'next-auth/react';
+import { SignInButton } from './sign-in-button';
+import { SignOutButton } from './sign-out-button';
+
 export function Header() {
+  const { data, status } = useSession();
+
   return (
-    <header className="flex w-full items-center justify-between bg-black p-4 text-white">
+    <header className="flex w-full items-center justify-between bg-slate-900 p-4 text-white">
       <div className="flex items-center justify-between gap-2">
         <CarFront size={25} />
         <h1 className="text-lg font-semibold">Rent-A-Car</h1>
       </div>
-      <div className="flex items-center justify-between gap-2">
-        <CircleUserRound size={25} />
-        <span className="font-semibold">John Doe</span>
-      </div>
+      {status === 'authenticated' ? (
+        <Popover>
+          <PopoverTrigger>
+            <Button className="text-md flex items-center gap-2 font-semibold">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={data.user?.image!} />
+                <AvatarFallback></AvatarFallback>
+              </Avatar>
+              <span>{data.user.name}</span>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-fit">
+            <SignOutButton />
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <SignInButton />
+      )}
     </header>
   );
 }
