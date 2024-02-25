9ef8abf7d46e7f9e49cb5504158bf0f9827b4202
Eric Oliveira <ericjmoliveira@gmail.com>
2024-02-25 18:53:22 -0300

    style: change header icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,31 @@
 'use client';
 
-import Link from 'next/link';
+import { BookText } from 'lucide-react';
 import { useSession } from 'next-auth/react';
-import { CarFront, BookText } from 'lucide-react';
+import Link from 'next/link';
 
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from './ui/button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { useState } from 'react';
+import { FaCar } from 'react-icons/fa';
 import { SignInButton } from './sign-in-button';
 import { SignOutButton } from './sign-out-button';
+import { Button } from './ui/button';
 
 export function Header() {
   const { data, status } = useSession();
+  const [popoverOpen, setPopoverOpen] = useState(false);
 
   return (
     <header className="flex w-full items-center justify-between bg-slate-900 p-4 text-white">
       <Link href={'/'}>
         <div className="flex items-center justify-between gap-2">
-          <CarFront size={25} />
+          <FaCar size={25} />
           <h1 className="text-lg font-semibold">Rent-a-Car</h1>
         </div>
       </Link>
       {status === 'authenticated' ? (
-        <Popover>
+        <Popover open={popoverOpen} onOpenChange={() => setPopoverOpen(true)}>
           <PopoverTrigger>
             <div className="text-md flex items-center gap-2 font-semibold">
               <Avatar className="h-8 w-8">
@@ -35,7 +38,9 @@ export function Header() {
           <PopoverContent className="w-fit">
             <Button variant="link" className="flex items-center gap-2">
               <BookText size={25} />
-              <Link href={'/reservations'}>View reservations</Link>
+              <Link href={'/reservations'} onClick={() => setPopoverOpen(false)}>
+                View reservations
+              </Link>
             </Button>
             <SignOutButton />
           </PopoverContent>
