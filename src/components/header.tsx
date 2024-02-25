ed402ac33db2e82d20bc02424730830e4ada644d
Eric Oliveira <ericjmoliveira@gmail.com>
2024-02-25 17:09:30 -0300

    style: update header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,10 +15,12 @@ export function Header() {
 
   return (
     <header className="flex w-full items-center justify-between bg-slate-900 p-4 text-white">
-      <div className="flex items-center justify-between gap-2">
-        <CarFront size={25} />
-        <h1 className="text-lg font-semibold">Rent-A-Car</h1>
-      </div>
+      <Link href={'/'}>
+        <div className="flex items-center justify-between gap-2">
+          <CarFront size={25} />
+          <h1 className="text-lg font-semibold">Rent-a-Car</h1>
+        </div>
+      </Link>
       {status === 'authenticated' ? (
         <Popover>
           <PopoverTrigger>
