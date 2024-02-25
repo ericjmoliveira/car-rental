805ce9cf3f7578115b54abac018b04226653666d
Eric Oliveira <ericjmoliveira@gmail.com>
2024-02-25 17:03:13 -0300

    feat: update create reservation action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,10 @@
 'use server';
 
+import { prisma } from './prisma';
+import { fetchCarClassById, fetchLocationById } from './data';
+import { getAuthenticatedUser } from './auth';
+import { calculateReservationTotalPrice } from '@/helpers/reservation';
+
 interface ReservationData {
   pickUpLocation: string;
   pickUpDate: Date;
@@ -9,5 +14,35 @@ interface ReservationData {
 }
 
 export async function createReservation(data: ReservationData) {
-  console.log(data);
+  const user = await getAuthenticatedUser();
+  const location = await fetchLocationById(data.pickUpLocation);
+  const carClass = await fetchCarClassById(data.carClass);
+
+  if (!user || !location || !carClass) return;
+
+  try {
+    if (!carClass) return;
+
+    const { rentTotalPrice } = calculateReservationTotalPrice({
+      pickupDate: data.pickUpDate,
+      returnDate: data.returnDate,
+      pricePerDay: carClass?.pricePerDay
+    });
+
+    const reservationData = {
+      userId: user?.id,
+      locationId: location?.id,
+      pickupDate: new Date(data.pickUpDate),
+      returnDate: new Date(data.returnDate),
+      classId: carClass?.id,
+      isYoungRenter: data.youngRenter,
+      totalPrice: data.youngRenter ? rentTotalPrice + 25 : rentTotalPrice
+    };
+
+    await prisma.reservation.create({ data: reservationData });
+  } catch (error) {
+    return {
+      message: 'An internal error occurred, try again later'
+    };
+  }
 }
