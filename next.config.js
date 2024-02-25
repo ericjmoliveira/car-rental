4a325941909ed1e39f5b6d2bae841c59852ad43f
Eric Oliveira <ericjmoliveira@gmail.com>
2024-02-25 17:02:42 -0300

    chore: config next.js image optimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.gcs.ehi.com',
+        port: '',
+        pathname: '/content/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.enterprise.com',
+        port: '',
+        pathname: '/en/**'
+      }
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
